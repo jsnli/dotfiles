0bf5d178cc50370f3ec5bafd1e64eb826f1b8134
Jason <130726285+jsnli@users.noreply.github.com>
2024-06-23 11:12:01 +0000

    Initial commit

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" }
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+		end
+	}
+}
