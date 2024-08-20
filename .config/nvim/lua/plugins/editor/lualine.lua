b0a9e94dbe8d18811f9ee833c12d31b8b3cbe389
Jason <130726285+jsnli@users.noreply.github.com>
2024-08-19 20:51:16 -0400

    update hypr and nvim

--- .config/nvim/lua/plugins/editor/lualine.lua
+++ .config/nvim/lua/plugins/editor/lualine.lua
@@ -1,9 +1,19 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "rose-pine",
-		},
-	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+			},
+			tabline = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {"buffers"},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }
