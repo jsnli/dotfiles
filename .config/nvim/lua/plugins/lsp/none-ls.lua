c95d2419089bb39c80e959d6c168f9b511d50385
Jason <130726285+jsnli@users.noreply.github.com>
2025-01-10 21:03:45 -0500

    update nvim

--- .config/nvim/lua/plugins/lsp/none-ls.lua
+++ .config/nvim/lua/plugins/lsp/none-ls.lua
@@ -9,7 +9,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 
